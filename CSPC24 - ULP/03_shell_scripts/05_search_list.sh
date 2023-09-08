eb65e4019b3e6772bf9b9fefa234e3267144fc18
Saksham Mittal <gotlouemail@gmail.com>
2023-09-08 14:55:52 +0530

    fix: read input from user in shell script

--- CSPC24 - ULP/03_shell_scripts/05_search_list.sh
+++ CSPC24 - ULP/03_shell_scripts/05_search_list.sh
@@ -1,11 +1,11 @@
 # Write a shell Script program to search whether the element is present is in the list or not.
 
 #!/bin/bash
-value = $1
 echo "Enter a value to check if it exists in the list --> "
+read -r value
 # read value
-arr = (10 3 2 34 543 5)
-if [[ " ${arr[*]} " =~ " ${value} " ]]; then
+arr=(10 3 2 34 543 5)
+if [[ "${arr[*]}" =~ $value ]]; then
 	echo "Element exists."
 
 else
